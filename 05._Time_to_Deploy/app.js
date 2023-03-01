fb85eea48e981663cacd57c5de4d5a31c08d9748
annesofiefogh <anne2z08@stud.kea.dk>
2023-03-01 17:47:39 +0100

    Calendar application first draft

---
+++ 05._Time_to_Deploy/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+
+/* pages */
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/frontpage/frontpage.html");
+});
+
+/* port */
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log("Server is running on port", PORT);
+});
