f389157c03ec3fa84e505276549913839a7b79f6
annesofiefogh <anne2z08@stud.kea.dk>
2023-03-12 14:22:54 +0100

    Redirection examples and import/export

--- 03._Plain_HTML_Server/app.js
+++ 03._Plain_HTML_Server/app.js
@@ -34,7 +34,7 @@ app.get("/museumGuards", (req, res) => {
 /* API */
 
 app.get("/api/tanks", (req, res) => {
-    res.send({ data: tanks });
+    res.send({ data: tanksUtil.getTanks() });
 });
 
 app.get("/api/visitors", (req, res) => {
@@ -44,6 +44,29 @@ app.get("/api/visitors", (req, res) => {
 app.put("/api/visitors", (req, res) => {
     res.send({ data: ++visitorCount });
 });
+
+// for SERVER SIDE REDIRECTION
+// how can you send data through a get request? (include in Mandatory 1) With query string or path variable
+/*app.get("/api/guards", (req, res) => {
+    // now the guard should take you to the tanks if you write localhost:8080/api/guards?passport=theskyisblue
+    if (req.query.passport === "theskyisblue") {
+        res.redirect("/api/tanks");
+    } else {
+        // the response to the browser so it doesn't hang and can't get any response is you type ../guards?passprt=theskyisnotblue
+        // | if this res.send isn't in an else the server will try to send 2 responses and it can't, so it will otherwise give an error
+        res.send({ message: "You are not allowed to see the tanks. Give us the secret in the query string with the key bein passport. "});
+    }
+});*/
+
+app.get("/api/guards", (req, res) => {
+    if (req.query.passport === "theskyisblue") {
+        // by adding a return here you don't have to but the res.send in an else like the above code^^
+        return res.redirect("/api/tanks");
+    } 
+    res.send({ message: "You are not allowed to see the tanks. Give us the secret in the query string with the key bein passport. "});
+});
+
+
 
 const PORT = 8080;
 app.listen(PORT, (error) => {
--- 03._Plain_HTML_Server/public/frontpage/frontpage.js
+++ 03._Plain_HTML_Server/public/frontpage/frontpage.js
@@ -1,1 +1,14 @@
 console.log("Thank you for visiting");
+
+// assignment redirect a user to /visitors after they've been to the museum for 3 seconds...
+// | write everything in this file
+
+// CLIENT SIDE REDIRECTION 
+window.setTimeout( () => {
+    window.location.href= "/visitors";
+    // another way: window.location.replace("/visitors")
+}, 3000);
+
+
+
+
